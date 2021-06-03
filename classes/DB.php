e2fea62210b901b0084783c59220e8d35b310a3f
Aleksej <32031682+rehkzylbz@users.noreply.github.com>
2021-06-03 20:28:01 +0300

    Update DB.php
    Чтение DB

--- classes/DB.php
+++ classes/DB.php
@@ -4,28 +4,28 @@ class DB {
 	
 	public $db;
     
-    public function __construct($settings) {
-        try {
-            $this->db = new PDO('mysql:dbname='.$settings['dbname'].';host='.$settings['host'], $settings['username'], $settings['password']);
-        } catch (PDOException $e) {
-            throw new Exception('Подключение к БД не удалось: ' . $e->getMessage());
-        }
-    }
-	
+	public function __construct($settings) {
+		try {
+		    $this->db = new PDO('mysql:dbname='.$settings['dbname'].';host='.$settings['host'], $settings['username'], $settings['password']);
+		} catch (PDOException $e) {
+		    throw new Exception('Подключение к БД не удалось: ' . $e->getMessage());
+		}
+	}
+
 	public function get_sizes($with_keys = false) {
-	    $result = [];
-        $query = $this->db->prepare('SELECT * FROM sizes ORDER BY id DESC');
-        if ( !$query->execute() ) {
-            throw new Exception('Запрос к БД не удался');    
-        }
-        else {
-            $result = $query->fetchAll();
-            if ( $with_keys ) {
-                $keys = array_column($result, 'name');
-                $result = array_combine($keys, $result);
-            }
-        }
-        return $result;
-    }
+		$result = [];
+		$query = $this->db->prepare('SELECT * FROM sizes ORDER BY id DESC');
+		if ( !$query->execute() ) {
+		    throw new Exception('Запрос к БД не удался');    
+		}
+		else {
+		    $result = $query->fetchAll();
+		    if ( $with_keys ) {
+			$keys = array_column($result, 'name');
+			$result = array_combine($keys, $result);
+		    }
+		}
+		return $result;
+	}
     
 }
